411d5cf99b3b9eb90cc46943b08e79fc1ac5aadd
Marcone Augusto <marcone.p.louzada@gmail.com>
2017-08-27 12:24:30 -0300

    Complexo + Principal

--- ex01/src/main/java/Complexo.java
+++ ex01/src/main/java/Complexo.java
@@ -1,2 +1,30 @@
 public class Complexo {
+
+    private int real;
+    private int imaginario;
+
+    public Complexo(){
+        real = 0;       //não precisaria ser declarado pois é executado implicitamente
+        imaginario = 0;     //variáveis inteiras são iniciadas em 0
+    }
+    public Complexo(int r , int i){
+        real = r;
+        imaginario = i;
+    }
+
+    public String toString(){
+        return  "(" +real+ "," +imaginario+ ")";
+    }
+
+    public void somaComplex(Complexo x,Complexo y) {
+        real = x.real + y.real;
+        imaginario = x.imaginario + y.imaginario;
+    }
+
+    public void subtraiComplex(Complexo x,Complexo y){
+        real = x.real - y.real;
+        imaginario = x.imaginario - y.imaginario;
+
+    }
 }
+
--- ex01/src/main/java/Principal.java
+++ ex01/src/main/java/Principal.java
@@ -1,2 +1,34 @@
 public class Principal {
+
+    public static void main(String[] args){
+
+        Complexo c1 = new Complexo();
+        Complexo c2 = new Complexo(3,5);
+        Complexo c3 = new Complexo(4,52);
+        Complexo c4 = new Complexo(2,33);
+
+        System.out.println("Complexo c1: " +c1);
+        System.out.println("Complexo c2: " +c2);
+        System.out.println("Complexo c3: " +c3);
+        System.out.println("Complexo c4: " +c4);
+
+        c1.somaComplex(c4,c3);
+        System.out.println("Complexo c1: "+c1);
+
+        c2.somaComplex(c1,c3);
+        System.out.println("Complexo c2: "+c2);
+
+        c3.somaComplex(c2,c4);
+        System.out.println("Complexo c3: "+c3);
+
+        c1.subtraiComplex(c2,c4);
+        System.out.println("Complexo c1: "+c1);
+
+        c3.subtraiComplex(c1,c3);
+        System.out.println("Complexo c3: "+c3);
+
+        c4.subtraiComplex(c4,c2);
+        System.out.println("Complexo c4: "+c4);
+
+    }
 }
